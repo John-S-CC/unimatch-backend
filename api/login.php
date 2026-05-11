0ede02dc8c74b33f62b958fe86e9949a46eb9181
John-S-CC <jsebastiancoronado@ucundinamarca.edu.co>
2026-05-11 18:46:15 -0500

    Remove CORS configuration from login.php
    Removed CORS headers and preflight OPTIONS handling.

--- api/login.php
+++ api/login.php
@@ -1,14 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: https://unimatch-frontend.onrender.com");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Credentials: true");
 
-// responder al preflight OPTIONS
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
 error_reporting(0);
 ini_set('display_errors', 0);
 
