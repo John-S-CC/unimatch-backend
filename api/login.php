f8f86553e83d33d10789349713aeef3870bf2b9d
John-S-CC <jsebastiancoronado@ucundinamarca.edu.co>
2026-05-18 19:38:37 -0500

    Add files via upload

--- api/login.php
+++ api/login.php
@@ -4,7 +4,7 @@ error_reporting(0);
 ini_set('display_errors', 0);
  require_once __DIR__ . "/_common.php";
 // require_once __DIR__ . "/../configuracion/database.php";
-// require_once __DIR__ . "/../configuracion/jwt.php";
+ require_once __DIR__ . "/../configuracion/jwt.php";
 
 echo json_encode([
     "ok" => true,
