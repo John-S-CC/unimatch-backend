f222368e62a3f4edc3102ceebdb999801a0d5dc1
John-S-CC <jsebastiancoronado@ucundinamarca.edu.co>
2026-05-23 22:33:52 -0500

    visualizar el error concreto 2

--- api/solicitar_recuperacion.php
+++ api/solicitar_recuperacion.php
@@ -30,7 +30,7 @@
  */
 error_reporting(0);
 ini_set('display_errors', 0);
-
+require_once __DIR__ . "/../vendor/autoload.php";
 require_once __DIR__ . "/_common.php";
 require_once __DIR__ . "/../configuracion/database.php";
 require_once __DIR__ . "/../servicios/Mailer.php";
